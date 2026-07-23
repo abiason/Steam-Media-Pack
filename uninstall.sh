20a77fde61aee1ef59e2b7c3f19403c581b6f225
Alberto Vieira de Sá Biason <abiason@gmail.com>
2026-07-23 16:00:50 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,9 @@ set -Eeuo pipefail
 DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
-rm -rf "$DATA_HOME/steam-media-pack-11"
-rm -rf "$CONFIG_HOME/steam-media-pack-11"
+rm -rf "$DATA_HOME/steam-media-pack"
+rm -rf "$CONFIG_HOME/steam-media-pack"
 rm -f "$DATA_HOME/applications"/steam-media-*.desktop
 rm -f "$DATA_HOME/icons/hicolor/scalable/apps"/steam-media-*.svg
 
-echo "Steam Media Pack 11.0 removido."
+echo "Steam Media Pack removido."
